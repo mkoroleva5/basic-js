764447dcdb4d08314a3f99fd4abd341f04982abd
Koroleva Maria <mkoroleva5@gmail.com>
2022-10-11 20:39:03 +0300

    feat: implement solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,24 +16,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function repeater(str, options) {
-  let arr = [];
-  while (options.repeatTimes > 0) {
-    arr.push(str);
-    if (options.repeatTimes > 1) {
-      if (!options.separator) arr.push('+');
-      else arr.push(options.separator);
-    }
-    while (options.additionRepeatTimes > 0) {
-      arr.push(options.addition);
-      if (options.additionRepeatTimes > 0) { 
-        if (!options.additionSeparator) arr.push('|')
-        else arr.push(options.additionSeparator);
-      }
-      options.additionRepeatTimes--;
-    }
-    options.repeatTimes--;
-  }
-  return arr.join('');
+  if (typeof options.repeatTimes === 'undefined') options.repeatTimes = 1;
+  if (typeof options.separator === 'undefined') options.separator = '+';
+  if (typeof options.addition === 'undefined') options.addition = '';
+  if (typeof options.additionRepeatTimes === 'undefined') options.additionRepeatTimes = 1;
+  if (typeof options.additionSeparator === 'undefined') options.additionSeparator = '|';
+  let res = `${str}${(options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1) + options.addition}${options.separator}`.repeat(options.repeatTimes);
+  return res.slice(0, res.lastIndexOf(options.separator));
 }
 
 module.exports = {
