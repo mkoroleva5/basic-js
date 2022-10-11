126455073b40ed3d88f5879dca25b3d39b788184
Koroleva Maria <mkoroleva5@gmail.com>
2022-10-11 17:49:35 +0300

    feat: implement solution for simple-chain

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let arr = [];
+  while (options.repeatTimes > 0) {
+    arr.push(str);
+    if (options.repeatTimes > 1) {
+      if (!options.separator) arr.push('+');
+      else arr.push(options.separator);
+    }
+    while (options.additionRepeatTimes > 0) {
+      arr.push(options.addition);
+      if (options.additionRepeatTimes > 0) { 
+        if (!options.additionSeparator) arr.push('|')
+        else arr.push(options.additionSeparator);
+      }
+      options.additionRepeatTimes--;
+    }
+    options.repeatTimes--;
+  }
+  return arr.join('');
 }
 
 module.exports = {
