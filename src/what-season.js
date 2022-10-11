1ed05f250aec6b525d84a4cd9a322501a5a23a29
Koroleva Maria <mkoroleva5@gmail.com>
2022-10-11 13:43:33 +0300

    feat: implement solution for hanoi-tower

--- src/what-season.js
+++ src/what-season.js
@@ -12,11 +12,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
- /* if (isNaN(date)) return 'Invalid date!';
-  else if (date.getMonth() === 11 || date.getMonth() === 0 || date.getMonth() === 1) return 'winter';
+  if (date.getMonth() === 11 || date.getMonth() === 0 || date.getMonth() === 1) return 'winter';
   else if (date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4) return 'spring';
   else if (date.getMonth() === 5 || date.getMonth() === 6 || date.getMonth() === 7) return 'summer';
-  else if (date.getMonth() === 8 || date.getMonth() === 9 || date.getMonth() === 10) return 'autumn';*/
+  else if (date.getMonth() === 8 || date.getMonth() === 9 || date.getMonth() === 10) return 'autumn';
+  else if (typeof date === 'object') return 'Unable to determine the time of year!';
+  else if (!date.contains('new Date')) return 'Invalid date!';
+
 }
 
 module.exports = {
